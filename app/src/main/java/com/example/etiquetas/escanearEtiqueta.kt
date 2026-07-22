009b08d94c1f7fa0cb1ee64f0bf8db605c4d6605
Jesus Carmona <ryzen3000@outlook.es>
2026-07-22 12:40:15 -0600

    separacion

--- app/src/main/java/com/example/etiquetas/escanearEtiqueta.kt
+++ app/src/main/java/com/example/etiquetas/escanearEtiqueta.kt
@@ -1,8 +1,11 @@
 package com.example.etiquetas
 
-import android.Manifest
+import android.annotation.SuppressLint
+import android.content.BroadcastReceiver
 import android.content.ContentValues
-import android.content.pm.PackageManager
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
@@ -10,28 +13,25 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TableLayout
 import android.widget.Toast
 import androidx.annotation.RequiresApi
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageAnalysis
-import androidx.camera.core.Preview
-import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.etiquetas.databinding.EscanearEtiquetaFragmentBinding
+import com.example.etiquetas.separador
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
+
 class EscanearEtiquetaFragment : Fragment() {
     private var _binding: EscanearEtiquetaFragmentBinding? = null
     private val binding get() = _binding!!
-
     private lateinit var cameraExecutor: ExecutorService
+    private val etiquetasEscaneadas = LinkedHashSet<Pair<String, String>>()
 
-    private val etiquetasEscaneadas =LinkedHashSet<Pair<String, String>>()
     @Volatile
     private var ultimaEtiquetaDetectada: String? = null
 
@@ -41,18 +41,13 @@ class EscanearEtiquetaFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = EscanearEtiquetaFragmentBinding.inflate(inflater, container, false)
+
         return binding.root
     }
 
     @RequiresApi(Build.VERSION_CODES.Q)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        if (allPermissionsGranted()) {
-            startCamera()
-        } else {
-            requestPermissions(REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
-        }
 
         cameraExecutor = Executors.newSingleThreadExecutor()
 
@@ -64,64 +59,54 @@ class EscanearEtiquetaFragment : Fragment() {
             guardarCSV()
         }
 
-        binding.scanBtn.setOnClickListener {
-            scanBtn()
-        }
     }
 
-    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
-        ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
-    }
-
-    private fun startCamera() {
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
-
-        cameraProviderFuture.addListener({
-            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
-
-            val preview = Preview.Builder()
-                .build()
-                .also {
-                    it.surfaceProvider = binding.cameraPreview.surfaceProvider
-                }
-
-            val imageAnalyzer = ImageAnalysis.Builder()
-                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST).build().also {
-                    it.setAnalyzer(cameraExecutor, barLector { etiqueta ->
-                        requireActivity().runOnUiThread {
-                            ultimaEtiquetaDetectada = etiqueta
-                        }
-                    })
-                }
-
-            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-
-            try {
-                cameraProvider.unbindAll()
-                cameraProvider.bindToLifecycle(
-                    viewLifecycleOwner, cameraSelector, preview, imageAnalyzer
-                )
-
-            } catch (exc: Exception) {
-                Log.e(TAG, "Algo falló, reintente", exc)
+    private val scanReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            Log.d(TAG, "Broadcast recibido: ${intent.extras?.keySet()?.joinToString()}")
+            val codigo = intent.getStringExtra("data")
+            if (!codigo.isNullOrEmpty()) {
+                guardarEtiqueta(codigo)
             }
-
-        }, ContextCompat.getMainExecutor(requireContext()))
+        }
     }
 
-    private fun scanBtn() {
-        val etiqueta = ultimaEtiquetaDetectada
+    @SuppressLint(
+        "UnspecifiedRegisterReceiverFlag",
+        "SuspiciousIndentation",
+        "IntentACTION_DATA_CODE_RECEIVED"
+    )
+    override fun onResume() {
+        super.onResume()
+        val filter = IntentFilter("com.sunmi.scanner.ACTION_DATA_CODE_RECEIVED")
+        requireContext().registerReceiver(scanReceiver, filter)
+        Log.d(TAG, "Receiver registrado, esperando broadcasts...")
+    }
 
-        if (etiqueta.isNullOrEmpty()) {
-            Toast.makeText(requireContext(), "No hay etiqueta que leer", Toast.LENGTH_SHORT).show()
-            return
-        }
+    override fun onPause() {
+        super.onPause()
+        requireContext().unregisterReceiver(scanReceiver)
+    }
 
+
+    private fun guardarEtiqueta(etiqueta: String) {
         val formato = SimpleDateFormat("dd/MM/yyyy HH:mm:ss", Locale.getDefault())
-        val fechaEscaneo = formato.format(Date())
-        etiquetasEscaneadas.add(etiqueta to fechaEscaneo)
-        binding.escaneos.text = etiquetasEscaneadas.joinToString("\n") { (valor, fecha) -> "$valor   ${fechaEscaneo}"  }
-        Toast.makeText(requireContext(), "Etiqueta guardada: $etiqueta", Toast.LENGTH_SHORT).show()
+        val fecha = formato.format(Date())
+        val util = separador()
+
+        val mensaje = util.etiquetaseparation(etiqueta)
+        println(mensaje)
+
+
+
+        etiquetasEscaneadas.add(etiqueta to fecha)
+
+
+        requireActivity().runOnUiThread {
+            binding.escaneos.text = etiquetasEscaneadas.joinToString("\n") { (valor, f) -> "$valor - $f" }
+
+
+        }
     }
 
     private fun cancelarEscaneo() {
@@ -135,7 +120,8 @@ class EscanearEtiquetaFragment : Fragment() {
     @RequiresApi(Build.VERSION_CODES.Q)
     private fun guardarCSV() {
         if (etiquetasEscaneadas.isEmpty()) {
-            Toast.makeText(requireContext(), "No hay etiquetas para guardar", Toast.LENGTH_SHORT).show()
+            Toast.makeText(requireContext(), "No hay etiquetas para guardar", Toast.LENGTH_SHORT)
+                .show()
             return
         }
 
@@ -158,32 +144,19 @@ class EscanearEtiquetaFragment : Fragment() {
                         outputStream.write("$valor,$fecha\n".toByteArray())
                     }
                 }
-                Toast.makeText(requireContext(), "CSV guardado en Descargas/Etiquetas", Toast.LENGTH_LONG).show()
+                Toast.makeText(
+                    requireContext(),
+                    "CSV guardado en Descargas/Etiquetas",
+                    Toast.LENGTH_LONG
+                ).show()
             } else {
-                Toast.makeText(requireContext(), "Error al crear el archivo", Toast.LENGTH_SHORT).show()
+                Toast.makeText(requireContext(), "Error al crear el archivo", Toast.LENGTH_SHORT)
+                    .show()
             }
         } catch (e: Exception) {
             Log.e(TAG, "Error al guardar CSV", e)
-            Toast.makeText(requireContext(), "Error al guardar: ${e.message}", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        if (requestCode == REQUEST_CODE_PERMISSIONS) {
-            if (allPermissionsGranted()) {
-                startCamera()
-            } else {
-                Toast.makeText(
-                    requireContext(),
-                    "Permisos no otorgados por el usuario.",
-                    Toast.LENGTH_SHORT
-                ).show()
-                requireActivity().finish()
-            }
+            Toast.makeText(requireContext(), "Error al guardar: ${e.message}", Toast.LENGTH_SHORT)
+                .show()
         }
     }
 
@@ -195,15 +168,5 @@ class EscanearEtiquetaFragment : Fragment() {
 
     companion object {
         private const val TAG = "Escanear Etiquetas"
-        private const val REQUEST_CODE_PERMISSIONS = 10
-        private val REQUIRED_PERMISSIONS =
-            mutableListOf(
-                Manifest.permission.CAMERA,
-                Manifest.permission.RECORD_AUDIO
-            ).apply {
-                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
-                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                }
-            }.toTypedArray()
     }
 }
